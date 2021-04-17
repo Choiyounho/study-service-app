4212ecfb0330aacf8a93d9b1c4d710c2a6f5c862
Choiyounho <maxosa78@gmail.com>
2021-04-18 00:55:18 +0900

    [feat] : 다른 아이디로 로그인 Issues

--- app/src/main/java/com/soten/androidstudio/j2kb/MainActivity.kt
+++ app/src/main/java/com/soten/androidstudio/j2kb/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.soten.androidstudio.j2kb
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.jakewharton.threetenabp.AndroidThreeTen
+import com.kakao.sdk.user.UserApiClient
 import com.soten.androidstudio.j2kb.utils.BackPressed
+import com.soten.androidstudio.j2kb.utils.CommonsConstant
+import com.soten.androidstudio.j2kb.utils.kakao.LoginActivity
 
 class MainActivity : AppCompatActivity(), BackPressed{
 
@@ -33,6 +41,29 @@ class MainActivity : AppCompatActivity(), BackPressed{
                 (fragment as BackPressed).backPress()
                 return
             }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_logout -> {
+                UserApiClient.instance.logout { error ->
+                    if (error != null) {
+                        Log.e(CommonsConstant.TAG, "로그아웃 실패. SDK에서 토큰 삭제됨", error)
+                    } else {
+                        Log.i(CommonsConstant.TAG, "로그아웃 성공. SDK에서 토큰 삭제됨")
+                        startActivity(Intent(this, LoginActivity::class.java))
+                    }
+                }
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 
