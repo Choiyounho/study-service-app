cb679136237a18574a5a4ea9b508f291e79f628b
Choiyounho <maxosa78@gmail.com>
2021-06-01 14:52:08 +0900

    [feat] : 파이어 베이스 데이터 입력 기능 구현

--- app/src/main/java/com/soten/androidstudio/j2kb/SplashActivity.kt
+++ app/src/main/java/com/soten/androidstudio/j2kb/SplashActivity.kt
@@ -2,10 +2,12 @@ package com.soten.androidstudio.j2kb
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import com.soten.androidstudio.j2kb.utils.CommonsConstant
 
 class SplashActivity : AppCompatActivity() {
 
