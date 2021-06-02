37971ef2d82e8098c7e35f6eb0138e365e9f2415
Choiyounho <maxosa78@gmail.com>
2021-06-02 18:17:14 +0900

    [refactor] : Member, 공지사항 리팩토링

--- app/src/main/java/com/soten/androidstudio/j2kb/model/post/NoticePost.kt
+++ app/src/main/java/com/soten/androidstudio/j2kb/model/post/NoticePost.kt
@@ -1,13 +1,14 @@
 package com.soten.androidstudio.j2kb.model.post
 
 import com.soten.androidstudio.j2kb.model.user.Role
+import com.soten.androidstudio.j2kb.utils.TimeFormat
 
 data class NoticePost(
-    val id: Long,
+    val id: String?,
     var title: String,
     var description: String,
     var nickname: String?,
     val grade: Role = Role.ADMIN,
-    val createdTime: String,
+    val createdTime: String = TimeFormat.createdTimeForNotice(),
     var updatedDate: String? = null
 )
