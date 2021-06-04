2cb6aaa1c4ec8ec28de6588dc25b17efd1b77271
Choiyounho <maxosa78@gmail.com>
2021-06-05 06:32:08 +0900

    [fix] : 화면 UI와 프로필 사진 등록 기능 수정

--- app/src/main/java/com/soten/androidstudio/j2kb/ui/home/qna/adapter/ChatItemAdapter.kt
+++ app/src/main/java/com/soten/androidstudio/j2kb/ui/home/qna/adapter/ChatItemAdapter.kt
@@ -13,8 +13,12 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.soten.androidstudio.j2kb.R
 import com.soten.androidstudio.j2kb.model.chat.ChatItem
+import com.soten.androidstudio.j2kb.utils.DBKey.Companion.DB_USERS
 
 class ChatItemAdapter : ListAdapter<ChatItem, ChatItemAdapter.ViewHolder>(diffUtil) {
 
@@ -31,12 +35,46 @@ class ChatItemAdapter : ListAdapter<ChatItem, ChatItemAdapter.ViewHolder>(diffUt
 
             Glide.with(thumbnail.context)
                 .load(chatItem.thumbnail)
-                .transform(CenterCrop(), RoundedCorners(dpToPx(thumbnail.context, CORNERS_DP)))
+                .transform(
+                    CenterCrop(),
+                    RoundedCorners(dpToPx(thumbnail.context, CORNERS_DP))
+                )
                 .into(thumbnail)
+            val store = Firebase.firestore
+            val auth = Firebase.auth
+            store.collection(DB_USERS)
+                .document(auth.currentUser?.uid.orEmpty())
+                .get()
+                .addOnSuccessListener {
+                    if (chatItem.id == auth.currentUser?.uid.orEmpty()) {
+                        Glide.with(thumbnail.context)
+                            .load(it["profileImage"])
+                            .transform(
+                                CenterCrop(),
+                                RoundedCorners(dpToPx(thumbnail.context, CORNERS_DP))
+                            )
+                            .into(thumbnail)
+                    } else {
+                        // 다른 사람 사진
+                        store.collection(DB_USERS)
+                            .document(chatItem.id)
+                            .get()
+                            .addOnSuccessListener { another ->
+                                Glide.with(thumbnail.context)
+                                    .load(another["profileImage"])
+                                    .transform(
+                                        CenterCrop(),
+                                        RoundedCorners(dpToPx(thumbnail.context, CORNERS_DP))
+                                    )
+                                    .into(thumbnail)
+                            }
+                    }
+                }
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatItemAdapter.ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):
+            ChatItemAdapter.ViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         return ViewHolder(inflater.inflate(R.layout.item_chat, parent, false))
     }
