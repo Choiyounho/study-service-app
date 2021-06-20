433f17b24a0ca105f5b5205dbabf49393f1c7aa6
Choiyounho <maxosa78@gmail.com>
2021-06-20 10:05:49 +0900

    [feat] : AddCardAdapter apply viewBinding

--- app/src/main/java/com/soten/androidstudio/j2kb/ui/home/goal/adapter/AddCardAdapter.kt
+++ app/src/main/java/com/soten/androidstudio/j2kb/ui/home/goal/adapter/AddCardAdapter.kt
@@ -5,18 +5,20 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.soten.androidstudio.j2kb.R
+import com.soten.androidstudio.j2kb.databinding.ItemAddCardBinding
 
 
 class AddCardAdapter(
     val onItemClicked: () -> Unit
-): RecyclerView.Adapter<AddCardAdapter.ViewHolder>() {
+) : RecyclerView.Adapter<AddCardAdapter.ViewHolder>() {
 
-    class ViewHolder(private val view: View): RecyclerView.ViewHolder(view) {}
+    class ViewHolder(binding: ItemAddCardBinding) : RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_add_card, parent, false)
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_add_card, parent, false)
 
-        return ViewHolder(view)
+        return ViewHolder(ItemAddCardBinding.bind(view))
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
