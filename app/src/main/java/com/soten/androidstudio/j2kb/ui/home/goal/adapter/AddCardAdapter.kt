c1558b2bbd4da82f19e74f68d41f2805a4d07863
Choiyounho <maxosa78@gmail.com>
2021-06-20 10:09:26 +0900

    [feat] : GoalCardAdapter apply viewBinding

--- app/src/main/java/com/soten/androidstudio/j2kb/ui/home/goal/adapter/AddCardAdapter.kt
+++ app/src/main/java/com/soten/androidstudio/j2kb/ui/home/goal/adapter/AddCardAdapter.kt
@@ -1,7 +1,6 @@
 package com.soten.androidstudio.j2kb.ui.home.goal.adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.soten.androidstudio.j2kb.R
--- app/src/main/java/com/soten/androidstudio/j2kb/ui/home/goal/adapter/GoalCardAdapter.kt
+++ app/src/main/java/com/soten/androidstudio/j2kb/ui/home/goal/adapter/GoalCardAdapter.kt
@@ -1,32 +1,33 @@
 package com.soten.androidstudio.j2kb.ui.home.goal.adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.soten.androidstudio.j2kb.R
+import com.soten.androidstudio.j2kb.databinding.ItemGoalCardBinding
 import com.soten.androidstudio.j2kb.model.goal.GoalCard
 
-class GoalCardAdapter: ListAdapter<GoalCard, GoalCardAdapter.ViewHolder>(diffUtil) {
+class GoalCardAdapter : ListAdapter<GoalCard, GoalCardAdapter.ViewHolder>(diffUtil) {
 
-    inner class ViewHolder(private val view: View): RecyclerView.ViewHolder(view) {
+    inner class ViewHolder(private val binding: ItemGoalCardBinding) :
+        RecyclerView.ViewHolder(binding.root) {
         fun bind(card: GoalCard) {
-            val name = view.findViewById<TextView>(R.id.nameTextView)
-            val firstGoal = view.findViewById<TextView>(R.id.firstGoalTextView)
-            val secondGoal = view.findViewById<TextView>(R.id.secondGoalTextView)
 
-            name.text = card.name
-            firstGoal.text = card.firstGoal
-            secondGoal.text = card.secondGoal
+            binding.nameTextView.text = card.name
+            binding.firstGoalTextView.text = card.firstGoal
+            binding.secondGoalTextView.text = card.secondGoal
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GoalCardAdapter.ViewHolder {
-        val inflater = LayoutInflater.from(parent.context)
-        return ViewHolder(inflater.inflate(R.layout.item_goal_card, parent, false))
+        return ViewHolder(
+            ItemGoalCardBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
     }
 
     override fun onBindViewHolder(holder: GoalCardAdapter.ViewHolder, position: Int) {
@@ -42,4 +43,5 @@ class GoalCardAdapter: ListAdapter<GoalCard, GoalCardAdapter.ViewHolder>(diffUti
                 oldItem == newItem
         }
     }
+
 }
